12f3ecc5389da40cae00ffb2eb6b65d3aabcf51d
Sergio Vera <severa@ebay.com>
2015-10-19 15:17:44 +0200

    Simplified function

--- game/game.go
+++ game/game.go
@@ -105,13 +105,11 @@ func (g *Game) GetMainStockHolders(corporation *corporation.Corporation) map[str
 func stockHoldersWithSameAmount(groupStart int, stockHolders []*player.Player, corporation *corporation.Corporation) []*player.Player {
 	group := []*player.Player{}
 	group = append(group, stockHolders[groupStart])
-	if groupStart+1 < len(stockHolders) && stockHolders[groupStart].Shares(corporation) == stockHolders[groupStart+1].Shares(corporation) {
-		group = append(group, stockHolders[groupStart+1])
-		i := groupStart + 2
-		for i < len(stockHolders) && stockHolders[groupStart] == stockHolders[i] {
-			group = append(group, stockHolders[i])
-			i++
-		}
+
+	i := groupStart + 1
+	for i < len(stockHolders) && stockHolders[groupStart].Shares(corporation) == stockHolders[i].Shares(corporation) {
+		group = append(group, stockHolders[i])
+		i++
 	}
 	return group
 }
